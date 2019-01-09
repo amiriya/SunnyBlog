f2524b07da0c82cf8ca6521421c1ab1f70a8b8a2
reyazhang <reyazhang@tencent.com>
2019-01-09 17:52:45 +0800

    optimize

--- react/src/components/ArticlePage/index.js
+++ react/src/components/ArticlePage/index.js
@@ -24,33 +24,41 @@ class ArticlePage extends Component {
   }
 
   render() {
-    const {title, author, createDate, browserCount, labels, markdownContent} = this.state.article;
-
+    const { title, markdownContent } = this.state.article;
+  
     return (
       <div className='article-page'>
         <h2 className="article-title">{title}</h2>
-        { Object.keys(this.state.article).length > 0 && <div className="article-info">
-          { author && <img src="images/orange/author_32.png" alt="作者" /> }
-          { author && <span className="author">{author}</span> }
-
-          { createDate && <img src="images/orange/time_32.png" alt="日期" /> }
-          { createDate && <span className="create_date">{createDate}</span> }
-
-          { browserCount && <img src="images/orange/browse_history_32.png" alt="浏览量" /> }
-          { browserCount && <span className="page_view">{browserCount}</span> }
-
-          {labels && labels.length > 0 && <span className="labels">
-            <img src="images/orange/label_32.png" alt="标签" />
-            {labels.map((label, index) => {
-              return (<a href={label.url} key={index}>{label.title}</a>);
-            })}
-          </span>}
-        </div> }
+        { this.getArticleInfo() }
         <div className="article-content">
           <ReactMarkdown source={markdownContent} />
         </div>
       </div>
     );
+  }
+
+  getArticleInfo(){
+    const { author, createDate, browserCount, labels } = this.state.article;
+
+    const  articleInfo = (<div className="article-info">
+      { !!author && <img src="images/orange/author_32.png" alt="作者" /> }
+      { !!author && <span className="author">{author}</span> }
+
+      { !!createDate && <img src="images/orange/time_32.png" alt="日期" /> }
+      { !!createDate && <span className="create_date">{createDate}</span> }
+
+      { !!browserCount && <img src="images/orange/browse_history_32.png" alt="浏览量" /> }
+      { !!browserCount && <span className="page_view">{browserCount}</span> }
+
+      {!!labels && labels.length > 0 && <span className="labels">
+        <img src="images/orange/label_32.png" alt="标签" />
+        {labels.map((label, index) => {
+          return (<a href={label.url} key={index}>{label.title}</a>);
+        })}
+      </span>}
+    </div>);
+
+    return Object.keys(this.state.article).length > 0 ? articleInfo : null;
   }
 }
 
